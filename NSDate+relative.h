4f6975a6f6c6f6b93ef3b99a62e06daddf794c30
Sami Samhuri <sami.samhuri@gmail.com>
2012-03-24 10:57:36 -0700

    support the retina iPad

---
+++ NSDate+relative.h
@@ -0,0 +1,15 @@
+//
+//  NSDate+relative.h
+//  DatingX
+//
+//  Created by Sami Samhuri on 11-08-25.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (NSDate_relative)
+
+- (NSString *) relativeToNow;
+
+@end
